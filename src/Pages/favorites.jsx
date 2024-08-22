e82b90e13ad4deee25705f6bf9d2f1b8211e796a
Mahmoud3mer <m.amer0440@gmail.com>
2024-08-22 18:13:24 +0300

    Update favorites page

--- src/Pages/favorites.jsx
+++ src/Pages/favorites.jsx
@@ -1,29 +1,39 @@
 import { useSelector } from "react-redux";
 import Card from "../Components/card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
 
 
 function Favorites(){
     const favorites = useSelector((state) => state.favorites.favoriteProducts);
     const favoritesNumber = favorites.length;
-    return(
-        <>
-            {favorites.length < 1 ? <div className="text-center w-full font-extrabold text-3xl font-mono bg-indigo-400 py-28">No Favorite Products</div> : 
-                <div className="grid sm:grid-cols-2 xs:grid-cols-1 gap-x-8 gap-y-8">
-                {favorites.map((favorite) => (
-                    <Card 
-                        key={favorite.id}
-                        image={favorite.images[1]}
-                        title={favorite.title}
-                        price={favorite.price}
-                        id={favorite.id}
-                        rate={favorite.ratingsAverage}
-                        currentProduct={favorite}
-                    />
-                ))}
-            </div>}
-            
-        </>
-    )
+
+    return (
+        <div className="container mx-auto my-8 px-4">
+            {favorites.length < 1 ? (
+                <div className="flex flex-col items-center justify-center w-full h-[50vh] bg-gradient-to-r from-indigo-400 to-purple-500 rounded-lg shadow-lg">
+                    <p className="text-white font-extrabold text-4xl mb-4">
+                        No Favorite Products
+                    </p>
+                </div>
+            ) : (
+                <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8">
+                    {favorites.map((favorite) => (
+                        <div key={favorite.id}>
+                            <Card 
+                                image={favorite.images[1]}
+                                title={favorite.title}
+                                price={favorite.price}
+                                id={favorite.id}
+                                rate={favorite.ratingsAverage}
+                                currentProduct={favorite}
+                            />
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Favorites;
